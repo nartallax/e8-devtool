4edda61115ab6c3970cc7fcc691786a275e6db1c
Nartallax <nartallax@gmail.com>
2024-06-28 08:05:35 +0200

    Rewrite hotkeys to be synchronous, for preventDefault() to work

--- src/client/components/hotkey_context/hotkey_context.tsx
+++ src/client/components/hotkey_context/hotkey_context.tsx
@@ -1,7 +1,6 @@
-import {CallResolver} from "client/components/hotkey_context/call_resolver"
 import {defineNestedTreeContext} from "client/ui_utils/define_nested_tree_context"
 import {noop} from "client/ui_utils/noop"
-import {useCallback, useEffect, useRef} from "react"
+import {RefObject, useCallback, useEffect, useMemo, useRef} from "react"
 
 export const enum ModalHotkeyPriority {
 	default = 0,
@@ -19,7 +18,28 @@ type Props = {
 	priority?: ModalHotkeyPriority
 }
 
-const keydownResolver = new CallResolver()
+type RootContextHandle = {
+	priority: number
+	getHandlers: () => HotkeyHandler[]
+	disabledCounterRef: RefObject<number>
+}
+let knownRoots: RootContextHandle[] = []
+
+const onWindowKeyDown = (e: KeyboardEvent) => {
+	const activeRoots = knownRoots.filter(x => x.disabledCounterRef.current === 0)
+	// this should pick one context with highest priority (maybe more, but when everything goes right - just one)
+	// most of the time multiple contexts are resolved by disabling, hovewer there are situations possible when contexts have the same parent
+	// for example, when a component defines a modal and an alert near that modal -
+	// both modal and alert (which is also a modal) will have their own hotkey contexts
+	// to resolve this, alert's hotkey context would have higher priority, because when alert is active - the screen is "blocked"
+	const highestPriority = activeRoots.map(x => x.priority).reduce((a, b) => Math.max(a, b), 0)
+	const selectedRoots = activeRoots.filter(x => x.priority === highestPriority)
+
+	const handlers = selectedRoots.flatMap(x => x.getHandlers()).filter(x => x.shouldPick && x.shouldPick(e))
+	for(const handler of handlers){
+		handler.onPress(e)
+	}
+}
 
 const {useMaybeRootContext, RootProvider, NestedProvider} = defineNestedTreeContext({
 	name: "HotkeyContext",
@@ -52,28 +72,24 @@ const {useMaybeRootContext, RootProvider, NestedProvider} = defineNestedTreeCont
 		const treeServiceRef = useRef(treeServices)
 		treeServiceRef.current = treeServices
 
+		const rootHandle: RootContextHandle = useMemo(() => ({
+			disabledCounterRef,
+			priority,
+			getHandlers: () => treeServiceRef.current.getSortedByDepth()
+		}), [priority])
+
 		useEffect(() => {
-			const rootHandler = async(e: KeyboardEvent) => {
-				if(disabledCounterRef.current > 0){
-					return
-				}
-
-				// this should pick one context with highest priority (maybe more, but when everything goes right - just one)
-				// most of the time multiple contexts are resolved by disabling, hovewer there are situations possible when contexts have the same parent
-				// for example, when a component defines a modal and an alert near that modal -
-				// both modal and alert (which is also a modal) will have their own hotkey contexts
-				// to resolve this, alert's hotkey context would have higher priority, because when alert is active - the screen is "blocked"
-				await keydownResolver.resolve(priority)
-
-				const handlers = treeServiceRef.current.getSortedByDepth(({shouldPick}) => !!shouldPick && shouldPick(e))
-				for(const handler of handlers){
-					handler.onPress(e)
+			knownRoots.push(rootHandle)
+			if(knownRoots.length === 1){
+				window.addEventListener("keydown", onWindowKeyDown)
+			}
+			return () => {
+				knownRoots = knownRoots.filter(x => x !== rootHandle)
+				if(knownRoots.length === 0){
+					window.removeEventListener("keydown", onWindowKeyDown)
 				}
 			}
-
-			window.addEventListener("keydown", rootHandler)
-			return () => window.removeEventListener("keydown", rootHandler)
-		}, [priority])
+		}, [rootHandle])
 
 		return {disable, enable}
 	}
